64677aad9567576739b4b0812bce3beaa8293355
Adrian Calavie <adrian.calavie06@gmail.com>
2025-11-20 07:43:34 +0200

    persist state across app restarts

--- src/stores/app-store.ts
+++ src/stores/app-store.ts
@@ -2,6 +2,7 @@ import { LoadingProgress } from "@/lib/types";
 import { Mod } from "@/models/mod";
 import { invoke } from "@tauri-apps/api/core";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type AppStoreState = {
   directory: string;
@@ -19,28 +20,38 @@ type AppStoreActions = {
 
 export type AppStore = AppStoreState & AppStoreActions;
 
-export const useAppStore = create<AppStore>()((set) => ({
-  directory: "",
-  mods: [],
-  modsLoading: false,
-  loadingProgress: null,
-  setDirectory: async (directory: string) => {
-    set({ directory });
+export const useAppStore = create<AppStore>()(
+  persist(
+    (set) => ({
+      directory: "",
+      mods: [],
+      modsLoading: false,
+      loadingProgress: null,
+      setDirectory: async (directory: string) => {
+        set({ directory });
 
-    set({ modsLoading: true });
-    const { mods } = await invoke<{ mods: Mod[] }>("load_mods", { directory });
-    set({ modsLoading: false });
+        set({ modsLoading: true });
+        const { mods } = await invoke<{ mods: Mod[] }>("load_mods", {
+          directory,
+        });
+        set({ modsLoading: false });
 
-    console.log(`Loaded ${mods.length} mods`);
-    set({ mods });
-  },
-  setMods: (mods: Mod[]) => {
-    set({ mods });
-  },
-  setProgress: (progress: LoadingProgress | null) => {
-    set({ loadingProgress: progress });
-  },
-  setLoading: (loading: boolean) => {
-    set({ modsLoading: loading });
-  },
-}));
+        console.log(`Loaded ${mods.length} mods`);
+        set({ mods });
+      },
+      setMods: (mods: Mod[]) => {
+        set({ mods });
+      },
+      setProgress: (progress: LoadingProgress | null) => {
+        set({ loadingProgress: progress });
+      },
+      setLoading: (loading: boolean) => {
+        set({ modsLoading: loading });
+      },
+    }),
+    {
+      name: "app-store",
+      partialize: (state) => ({ directory: state.directory, mods: state.mods }),
+    },
+  ),
+);
